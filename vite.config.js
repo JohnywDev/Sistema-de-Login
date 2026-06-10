b7c66fb696a14c008d419156047ff42b9e6cc78c
Johny <johnyvozon@gmail.com>
2026-06-09 23:22:13 -0300

    vitconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' 
+import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/Sistema-de-Login/', 
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Isso ativa o React Compiler que está no seu package.json
+    babel({
+      plugins: [
+        ['babel-plugin-react-compiler', { target: '19' }]
+      ]
+    })
+  ]
 })
