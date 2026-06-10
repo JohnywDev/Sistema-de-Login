8c04f5f14f6204a6dedf21cfe0b869c23e8a2d9b
Johny <johnyvozon@gmail.com>
2026-06-09 22:36:17 -0300

    arrumando arquivos raizes para rodar no git

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,5 @@ function App() {
   )
 }
 
-
 export default App
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://johnywdev.github.io/Sistema-de-Login/', //adicione o nome exato do seu projeto no git aqui
-})
 
